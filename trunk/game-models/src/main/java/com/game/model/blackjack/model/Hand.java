1ea8eb65d3e875c47a92a93a9563dc37b04e47e2
djhelbert@gmail.com <djhelbert@gmail.com@77daecbf-9f6b-5650-f5d7-1a5980a03f0d>
2012-01-22 06:19:35 +0000

    Added amounts for wagers

--- trunk/game-models/src/main/java/com/game/model/blackjack/model/Hand.java
+++ trunk/game-models/src/main/java/com/game/model/blackjack/model/Hand.java
@@ -2,6 +2,7 @@ package com.game.model.blackjack.model;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 import com.game.model.common.Card;
 import com.game.model.common.Card.Type;
@@ -16,6 +17,51 @@ public class Hand {
 
 	private List<Card> cards = new ArrayList<Card>();
 
+	private int bet = 0;
+	
+	private UUID uuid = UUID.randomUUID();
+
+	/**
+	 * Constructor
+	 * 
+	 */
+	public Hand() {
+	}
+	
+	/**
+	 * Constructor
+	 * 
+	 * @param id
+	 */
+	public Hand(int bet) {
+		this.bet = bet;
+	}
+	
+	/**
+	 * Add Card
+	 * 
+	 * @param card
+	 */
+	public void addCard(Card card) {
+		cards.add(card);
+	}
+	
+	/**
+	 * Clear
+	 */
+	public void clear() {
+		cards.clear();
+	}
+	
+	/**
+	 * Get UUID
+	 * 
+	 * @return
+	 */
+	public UUID getUuid() {
+		return uuid;
+	}
+	
 	/**
 	 * Get Cards
 	 * 
@@ -34,6 +80,11 @@ public class Hand {
 		this.cards = cards;
 	}
 
+	/**
+	 * Get Maximum Value
+	 * 
+	 * @return
+	 */
 	public int getMaximumValue() {
 		int     temp = 0;
 		boolean aces = false;
@@ -53,7 +104,12 @@ public class Hand {
 		return temp;
 	}
 	
-	public int getTotalValue() {
+	/**
+	 * Get Soft Value
+	 * 
+	 * @return
+	 */
+	public int getSoftValue() {
 		int temp = 0;
 		
 		for(Card c : cards) {
@@ -61,5 +117,45 @@ public class Hand {
 		}
 		
 		return temp;
+	}
+	
+	/**
+	 * Get Number of Cards
+	 * 
+	 * @return int
+	 */
+	public int getNumberCards() {
+		return cards.size();
+	}
+	
+	public boolean isBusted() {
+		return getSoftValue() > 21;
+	}
+
+	/**
+	 * Get Bet
+	 * 
+	 * @return
+	 */
+	public int getBet() {
+		return bet;
+	}
+
+	/**
+	 * Set Bet
+	 * 
+	 * @param bet
+	 */
+	public void setBet(int bet) {
+		this.bet = bet;
+	}
+	
+	@Override
+	public boolean equals(Object object) {
+		if(object instanceof Hand) {
+			return uuid.equals(((Hand) object).getUuid());
+		}
+		
+		return false;
 	}
 }
